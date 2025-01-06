d41f2f30509602e7f89b9f4b5bbf81da7bde8760
Maeva Bouvard <maevabouvard@gmail.com>
2025-01-06 20:37:14 +0100

    Delete recaptcha secret key from captcha.php

--- views/auth/captcha.php
+++ views/auth/captcha.php
@@ -1,5 +1,5 @@
 <?php 
-$recaptcha_secret = "6Lf6rK8qAAAAAGcrt68iwQUi-PEz-prbGy2B4sTj";
+$recaptcha_secret = "recaptcha_secret_key"; // A remplacer par la clé secrète
 $response = $_POST['g-recaptcha-response'];
 $verify = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$recaptcha_secret}&response={$response}");
 $captcha_success = json_decode($verify);
